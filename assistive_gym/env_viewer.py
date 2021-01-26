a37ef1787d54d7958264f6ee88d5f4e67039a583
Zackory <zackory@gatech.edu>
2021-01-25 22:18:33 -0500

    Updated env_viewer to visualize active human environments

--- assistive_gym/env_viewer.py
+++ assistive_gym/env_viewer.py
@@ -1,22 +1,35 @@
 import gym, sys, argparse
 import numpy as np
+from .learn import make_env
 # import assistive_gym
 
 if sys.version_info < (3, 0):
     print('Please use Python 3')
     exit()
 
+def sample_action(env, coop):
+    if coop:
+        return {'robot': env.action_space_robot.sample(), 'human': env.action_space_human.sample()}
+    return env.action_space.sample()
+
 def viewer(env_name):
-    env = gym.make(env_name)
+    coop = 'Human' in env_name
+    env = make_env(env_name, coop=True) if coop else gym.make(env_name)
 
     while True:
         done = False
         env.render()
         observation = env.reset()
-        action = env.action_space.sample()
-        print('Observation size:', np.shape(observation), 'Action size:', np.shape(action))
+        action = sample_action(env, coop)
+        if coop:
+            print('Robot observation size:', np.shape(observation['robot']), 'Human observation size:', np.shape(observation['human']), 'Robot action size:', np.shape(action['robot']), 'Human action size:', np.shape(action['human']))
+        else:
+            print('Observation size:', np.shape(observation), 'Action size:', np.shape(action))
+
         while not done:
-            observation, reward, done, info = env.step(env.action_space.sample())
+            observation, reward, done, info = env.step(sample_action(env, coop))
+            if coop:
+                done = done['__all__']
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Assistive Gym Environment Viewer')
