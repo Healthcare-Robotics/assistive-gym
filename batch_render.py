08bdb2a0aa003bf8adbdd9d0ebba4fb767f907f9
Louis Nguyen <ndlong95@gmail.com>
2023-10-24 19:20:29 -0400

    Fix save trainresult (#19)
    * using wrapper object for mainEnvProcess
    
    * make typesafe with static typing on mptrain. also, update readme
    
    * fix train result not save after refactoring

--- batch_render.py
+++ batch_render.py
@@ -6,18 +6,18 @@ from assistive_gym.train import render, render_pose
 PERSON_IDS = [ 'p001']
 # PERSON_IDS = ['p004']
 # SMPL_FILES = ['s01' ]
-# SMPL_FILES = ['s01', 's02', 's03', 's04', 's05', 's06', 's07', 's08', 's09', 's10', 's11', 's12',
-#               's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21', 's22', 's23', 's24', 's25', 's26',
-#               's27', 's28', 's29', 's30', 's31', 's32', 's33', 's34', 's35', 's36', 's37', 's38', 's39', 's40',
-#               's41', 's42', 's43', 's44', 's45']
-SMPL_FILES = [ 's18']
+SMPL_FILES = ['s01', 's02', 's03', 's04', 's05', 's06', 's07', 's08', 's09', 's10', 's11', 's12',
+              's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21', 's22', 's23', 's24', 's25', 's26',
+              's27', 's28', 's29', 's30', 's31', 's32', 's33', 's34', 's35', 's36', 's37', 's38', 's39', 's40',
+              's41', 's42', 's43', 's44', 's45']
+# SMPL_FILES = [ 'S01']
 # SMPL_FILES = [ 's44']
 # PERSON_IDS = [ 'p001', 'p002']
 # SMPL_FILES = [ 's19', 's20', 's44', 's45']
 OBJECTS = ['cane', 'cup', 'pill']
 #### Define static configs ####
 SMPL_DIR = 'examples/data/slp3d/'
-ENV = "HumanComfort-v1_param_1509_yaml"
+ENV = "HumanComfort-v1"
 SEED = 1001
 SAVE_DIR = 'trained_models'
 SIMULATE_COLLISION = False
